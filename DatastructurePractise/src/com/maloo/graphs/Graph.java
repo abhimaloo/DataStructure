a324d6cfba9df708bd8d1f74cafcadea5e768b3d
abhimaloo <abhimaloo@gmail.com>
2014-07-01 13:27:11 -0700

    Adding Normal Graph Code

---
+++ DatastructurePractise/src/com/maloo/graphs/Graph.java
@@ -0,0 +1,121 @@
+package com.maloo.graphs;
+
+import java.util.*;
+
+/**
+ * Created by abhishekm787 on 6/30/14.
+ */
+public class Graph {
+
+   private Map<Integer,List<Edge>> store = new Hashtable<>();
+
+   public void addVertex(int vertex) {
+       if(!contains(vertex)){
+           store.put(vertex,new ArrayList<Edge>());
+       }
+   }
+
+   public List<Edge> getEdges(int vertex) {
+       return store.get(vertex);
+   }
+
+   public Set<Integer> getAllVertex(){
+       return store.keySet();
+   }
+
+   public List<Edge> getAllEdges() {
+       List<Edge> response = new ArrayList<>();
+       for(Integer key:store.keySet()) {
+           response.addAll(store.get(key));
+       }
+
+       return response;
+   }
+
+   public boolean contains(int vertex) {
+        return store.containsKey(vertex);
+   }
+
+   public void addEdge(int from, int to, int weight) {
+
+          if(!contains(to)){
+              addVertex(to);
+          }
+
+          if(!contains(from)){
+              addVertex(from);
+          }
+
+          store.get(from).add(new Edge(to, from, weight));
+   }
+
+
+   public void dfs(int startPoint, Set<Integer> visited){
+
+       if(!visited.contains(startPoint)){
+           //visit it
+           System.out.println(startPoint);
+           visited.add(startPoint);
+       }
+
+       //get all out going edges from start point
+       for(Edge e: getEdges(startPoint)){
+           dfs(e.to,visited);
+       }
+
+   }
+
+
+   public void bfs(int startpoint) {
+       LinkedList<Integer> queue = new LinkedList<>();
+       Set<Integer> visited = new HashSet<>();
+
+       //add the start point to queue
+       queue.addLast(startpoint);
+
+       while(!queue.isEmpty()){
+           int vertex = queue.removeFirst() ;
+           if(!visited.contains(vertex)){
+               //visit the vertex
+               System.out.println(vertex);
+               visited.add(vertex);
+           }
+           // add the next neighbours
+           for (Edge e: getEdges(vertex)){
+               queue.add(e.to);
+           }
+
+       }
+
+
+
+   }
+
+
+    public static void main(String[] args) {
+        Graph g = new Graph();
+
+        g.addEdge(1,2,2);
+        g.addEdge(2,3,5);
+        g.addEdge(3,4,2);
+
+        g.bfs(1);
+
+    }
+
+
+    class Edge {
+        public int to;
+        public int from;
+        public int weight;
+
+        Edge(int to, int from, int weight) {
+            this.to = to;
+            this.from = from;
+            this.weight = weight;
+        }
+    }
+
+
+
+}
