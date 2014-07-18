9c752033dce9114cb0747b59d42e2cc6bb9c158e
abhimaloo <abhimaloo@gmail.com>
2014-07-17 20:04:42 -0700

    more DP questions

--- DatastructurePractise/src/com/interview/arrays/dp/LongestCommonSubstring.java
+++ DatastructurePractise/src/com/interview/arrays/dp/LongestCommonSubstring.java
@@ -1,23 +1,60 @@
 package com.interview.arrays.dp;
 
-import com.maloo.trees.TrieTree;
-
 /**
  * find the longest common substring between two strings "abcde" and "xabcz".
  * result should be "abc"
  * Created by abhishekm787 on 7/16/14.
  */
 public class LongestCommonSubstring {
-    public static String findLongestCommonSubstringUsingTries(String a, String b) {
-        TrieTree suffix = new TrieTree();
-        suffix.insert(suffix.root, a);
-        suffix.insert(suffix.root,b);
 
+    /**
+     * trick is to use DP
+     * maintain a length[m][n] array which contains length of common substring from
+     * m mean 0...m and n mean 0..n indexes
+     *
+     * @param s1
+     * @param s2
+     * @return
+     */
+    public static char[] findLongestCommonSubstringUsingTries(char[] s1, char[] s2) {
+        int[][] length = new int[s1.length][s2.length];
+        int maxLength = 0; int maxi=-1;
 
-        return null;
+        for(int i=0;i<s1.length; i++){
+            for( int j=0; j< s2.length; j++) {
+                // if the characters match
+                if(s1[i] == s2[j]){
+                    // increase the length by 1 since i-1 and j-1 substrings
+                    length[i][j] = 1+ (i-1>=0 && j-1>=0 ? length[i-1][j-1]: 0);
+                    //reconstruction logic
+                    if(maxLength < length[i][j]) {
+                        maxLength = length[i][j];
+                        maxi = i;
+                    }
+
+                } else {
+                    //otherwise find maximum of matching length by adding the last character on both the sides
+                    length[i][j] = 0;
+                }
+
+            }
+        }
+
+        char[] response = new char[maxLength];
+        while(maxLength>0){
+            response[maxLength-1] = s1[maxi];
+            maxLength--;
+            maxi--;
+        }
+
+      return response;
     }
 
     public static void main(String[] args) {
-        System.out.println(findLongestCommonSubstringUsingTries("abcde", "xabcz"));
+        char[] lcs = findLongestCommonSubstringUsingTries("abcde".toCharArray(), "xabcz".toCharArray());
+        for (int i = 0; i < lcs.length; i++) {
+            System.out.println(lcs[i]);
+
+        }
     }
 }
