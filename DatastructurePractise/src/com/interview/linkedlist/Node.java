e9fde2471d542fbb6878d65b1ec2d557cd258fb8
abhimaloo@gmail.com <Database22>
2014-08-23 00:52:47 -0700

    LRU cache and Merge sort linked list added

--- DatastructurePractise/src/com/interview/linkedlist/Node.java
+++ DatastructurePractise/src/com/interview/linkedlist/Node.java
@@ -4,8 +4,8 @@ package com.interview.linkedlist;
  * Created by abhishekm787 on 7/28/14.
  */
 public class Node {
-    int data;
-     Node next;
+    public int data;
+     public Node next;
 
     public Node(int data, Node next) {
         this.data = data;
